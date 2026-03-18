55d03a51c7ea7cff53fccb290dd66c0852b46eac
Niels Ørbæk Chemnitz <nielsoerbaek@gmail.com>
2026-03-18 16:38:21 +0100

    feat: add preview refresh button, rename modal, and breadcrumbs

--- apps/bulk_email/urls.py
+++ apps/bulk_email/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path("send/", views.BulkEmailSendView.as_view(), name="send"),
     path("attachments/<int:pk>/", views.BulkEmailAttachmentDownloadView.as_view(), name="attachment_download"),
     path("<int:pk>/slet/", views.BulkEmailDeleteView.as_view(), name="delete"),
+    path("<int:pk>/omdoeb/", views.BulkEmailRenameView.as_view(), name="rename"),
 ]
